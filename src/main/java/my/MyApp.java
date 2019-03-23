5852eea1eddc697099fef6f5ff22cf5a14e07422
Hetao <hetao@zqykj.com>
2019-03-23 16:45:30 +0800

    spring skeleton

---
+++ src/main/java/my/MyApp.java
@@ -0,0 +1,26 @@
+package my;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+
+@SpringBootApplication
+@ComponentScan({ "my" })
+public class MyApp  implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApp.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //TODO
+    }
+
+
+}
